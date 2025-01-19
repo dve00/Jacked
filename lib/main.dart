38ae967d842e315a49ba061570f5001b8079fe0b
Dario Veltri <dario.veltri@rwth-aachen.de>
2025-01-19 18:50:34 +0100

    Added Navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:jacked/pages/diary_page.dart';
+import 'package:jacked/pages/exercises_page.dart';
+import 'package:jacked/pages/program_page.dart';
+import 'package:jacked/pages/workout_page.dart';
+import 'package:jacked/pages/you_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +18,8 @@ class MyApp extends StatelessWidget {
       title: 'Jacked',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 218, 130, 8)),
+          seedColor: Colors.amber,
+        ),
         useMaterial3: true,
       ),
       home: const JackedHomePage(),
@@ -21,16 +27,53 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class JackedHomePage extends StatelessWidget {
+class JackedHomePage extends StatefulWidget {
   const JackedHomePage({super.key});
+
+  @override
+  State<JackedHomePage> createState() => _JackedHomePageState();
+}
+
+class _JackedHomePageState extends State<JackedHomePage> {
+  int currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Center(),
+      bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (index) => setState(() {
+                currentPageIndex = index;
+              }),
+          selectedIndex: currentPageIndex,
+          destinations: const <Widget>[
+            NavigationDestination(
+                icon: Icon(Icons.person_2_outlined),
+                selectedIcon: Icon(Icons.person_2),
+                label: "You"),
+            NavigationDestination(
+                icon: Icon(Icons.auto_stories_outlined),
+                selectedIcon: Icon(Icons.auto_stories),
+                label: "Diary"),
+            NavigationDestination(
+                icon: Icon(Icons.add_box_outlined),
+                selectedIcon: Icon(Icons.add_box),
+                label: "Workout"),
+            NavigationDestination(
+                icon: Icon(Icons.edit_calendar_outlined),
+                selectedIcon: Icon(Icons.edit_calendar),
+                label: "Program"),
+            NavigationDestination(
+                icon: Icon(Icons.fitness_center_outlined),
+                selectedIcon: Icon(Icons.fitness_center),
+                label: "Exercises")
+          ]),
+      body: <Widget>[
+        YouPage(),
+        DiaryPage(),
+        WorkoutPage(),
+        ProgramPage(),
+        ExercisesPage(),
+      ][currentPageIndex],
     );
   }
 }
---
+++ lib/pages/program_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProgramPage extends StatelessWidget {
+  const ProgramPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Program')),
+    );
+  }
+}
